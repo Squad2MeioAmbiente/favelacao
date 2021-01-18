264dd4423be2db1ab785762bd5254fd8810f083a
Ruan <devd8727@gmail.com>
2021-01-18 17:35:41 -0300

    .

--- frontend/screen1/s1.js
+++ frontend/screen1/s1.js
@@ -1,47 +1,4 @@
 
-// var dados
-// async function getText() {
-//     var response = await fetch("http://projetos/USEPHPAQUI/MEUfseletro/em%20react/Back-End/") 
-//      dados = await response.json()
-//     console.log(dados)
-       
-// }
-// console.log(dados)
-// // console.log(dados)
-// var dados
-// async function getText() {
-//     fetch("http://projetos/USEPHPAQUI/MEUfseletro/em%20react/Back-End/")
-//     .then(res => res.json() ) .then(res => dados = res)
-// console.log(dados)
-
-// }
-
-
-// var frases = [{"texto":"Frase1"},{"texto":"Frase2"},{"texto":"Frase3"},{"texto":"Frase4"},{"texto":"Frase5"},]
-
-// var frases = [
-//     {
-//     "idcliente": "1",
-//     "nome": "Ruan Lima ",
-//     "email": "devd8727@gmail.com",
-//     "produto": "Geladeira frost free",
-//     "review": "Muito boa, consome pouca energia e é super espaçosa"
-//     },
-//     {
-//     "idcliente": "2",
-//     "nome": "josé",
-//     "email": "josé123@gmail.com",
-//     "produto": "Microondas",
-//     "review": "Muito útil. Coube certinho aqui na prateleira de casa!"
-//     },
-//     {
-//     "idcliente": "3",
-//     "nome": "Luan",
-//     "email": "luangameplay@gmail.com",
-//     "produto": "Fogão 4 Bocas",
-//     "review": "Adorei!! Super econômico"
-//     }
-//     ]
  var count = 0
 
 async function next (dados){
