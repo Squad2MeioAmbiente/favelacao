2c8954c7356726472dacc6c95f2c3cb30c9cc209
Ruan <devd8727@gmail.com>
2021-01-19 02:45:14 -0300

    Lógica para trocar personagem + fundo e de sons

--- frontend/screen1/s1.js
+++ frontend/screen1/s1.js
@@ -1,18 +1,48 @@
 
  var count = 0
 
-async function next (dados){
+async function next (){
+    
   var response = await fetch("http://projetos/entregaGame/api/enredo/read.php") 
      dados = await response.json()
-    console.log(dados)
+   
 
   var txt =document.getElementById("txt")
-
   txt.innerHTML = dados[count].frase
 
 
+  var img = document.getElementById("teste")
+  img.src= "http://projetos/entregaGame/frontend/img/franco.jpg"
+
+  var fundo = document.getElementById("fundo")
+  if (count == 9){
+    fundo.style.backgroundImage = "url('http://projetos/entregaGame/frontend/img/background/b2.jpg')";}
+    else if (count == 12){
+      fundo.style.backgroundImage = "url('http://projetos/entregaGame/frontend/img/background/b3.jpg')";
+    }
+
   count ++
   console.log(count)
- 
+
 }
-    
+var contador = 0
+ function mute () {
+  var turn = document.getElementById("turn")
+  var mute = document.getElementById("mudo")
+   if (contador == 0){
+      mute.pause()
+      contador = 1
+      turn.innerHTML = "Ligar Música"
+   }
+   else {
+     mute.play()
+     contador = 0
+     turn.innerHTML = "Desligar Música"
+   } 
+}
+
+function btnsound(){
+  var btnsound = document.getElementById("btnsound")
+  btnsound.play()
+}
+  
