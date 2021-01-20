837a2d77bd704e3a3e86083b8e57290ad5e12658
Suh Moraes <suh.alvesmoraes@gmai.com>
2021-01-20 11:30:17 -0300

    Form Responsive �

---
+++ perfil.js
@@ -0,0 +1,28 @@
+
+async function get(){
+   var response =  await fetch("http://projetos/PerfilSuh/backpathy/api/user/read.php")
+   dados = await response.json()
+   console.log(dados)
+
+   var nome = document.getElementById("nome")
+   var idade = document.getElementById("idade")
+   var email = document.getElementById("email")
+   var apelido = document.getElementById("apelido")
+   nome.innerHTML = dados[0].nome
+   idade.innerHTML = dados[0].dataNascimento
+   email.innerHTML = dados[0].email
+   apelido.innerHTML = dados[0].apelido
+   
+   console.log(nome)
+   console.log(idade)
+   console.log(email)
+   console.log(apelido)
+   
+}
+
+function update(){
+
+   
+     
+      
+   }
