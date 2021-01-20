40f0fbfc1eb5d5255f88d4d042fa22c4a23cb44a
PatriciaTorresGraciano <patricia.dstorres@hotmail.com>
2021-01-20 00:54:15 -0300

    Subindo alteração com menu

---
+++ frontend/home.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!--CSS/Bootstrap-->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> 
+  <!--CSS-->
+  
+  <link rel="stylesheet" href="css/style.css">
+  <!--Fonts-->
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700;900&display=swap" rel="stylesheet">
+  <link rel="sortcut icon" href="./img/logo_Megafone_Grande.png" type="image/x-icon" />
+
+  <title>Home</title>
+</head>
+
+<body>
+<header>
+
+<?php
+    include("./templates/menu.html")
+?>
+
+</header>
+
+  <div class=" bg-image fundoAzul">  
+    <img  class="personagem" src="./img/helio_saudacao2.svg" alt="Hélio">  
+    <div class="textoBanner">
+      <img   src="./img/textoBanner2.svg" alt="Texto">
+      <button class="buttonJogar">JOGAR</button>
+    </div> 
+  </div>
+  <div class="textDescricao">
+    <h1 class="tituloFavelacao">O que é o FavelAção? </h1>
+    <p class="descricaoFavelacao">
+      Os moradores de uma comunidade estão tendo um grande problema com lixo ao céu aberto. <br>
+     Para solucionar esse problema Hélio e sua turma decidem ajudar, porém sua turma é pequena, <br>
+     então Hélio recruta mais crianças no FavelAção para combater esse mal.
+      <br><br>
+     Você é a próxima criança, topa fazer parte dessa aventura?
+    </p>
+  </div>
+  <div class="premio">
+    
+      <h1> Aprenda com o FavelAção</h1>
+
+      <div class="premioIcons">
+        <div class="favelaIcon">
+          <img src="./img/favela_icon.png" alt="Favela">
+          <p>
+            O único  jogo <br>
+            que acontece <br>
+            em Heliópolis
+          </p>
+        </div>
+        <div class="medalIcon">
+          <img src="./img/Icon awesome-medal.png" alt="Medalha">
+          <p>            
+            Ganhe medalhas <br>
+            ao concluir cada <br>
+            missão
+          </p>
+        </div>
+        <div class="rankingIcon">
+          <img src="./img/ranking_icon.png" alt="Ranking">
+          <p>
+            Seja o que mais <br>
+            ajuda sua <br>
+            comunidade
+          </p>
+        </div>
+      </div>
+  </div>
+  <div class="missao">
+    <h1>Missões</h1>
+
+    <div class="missaoBox">
+      <div class="card " >
+        <img src="#" class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Cena do Jogo 1</p>
+        </div>
+      </div>
+      <div class="card " >
+        <img src="#" class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Cena do Jogo 2</p>
+        </div>
+      </div>
+      <div class="card " >
+        <img src="#" class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Cena do Jogo 3</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+
+  <footer class="rodape">
+    
+    <a href="https://instagram.com/">
+      <img class="iconInstagram" src="./img/instagram.svg" alt="Instagram"> 
+    </a>
+    <a href="https://github.com/">
+      <img class="iconGit"src="./img/logotipo-do-github.svg" alt="GitHub">
+    </a>
+    <a href="https://www.instagram.com/">
+      <img class="iconInstagram" src="./img/facebook.svg" alt="">
+    </a> 
+    <span>FavelAção 2021</span>
+  </footer>
+          
+         
+
+   <!--JavaScript/Bootstrap-->
+   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ frontend/jogo/screen1/s1.js
@@ -0,0 +1,53 @@
+
+ var count = 0
+
+async function next (){
+    
+  var response = await fetch("http://localhost/favelacao/api/enredo/read.php") 
+     dados = await response.json()
+   
+
+  var txt =document.getElementById("txt")
+  txt.innerHTML = dados[count].frase
+
+
+  var img = document.getElementById("teste")
+  var caminho = `../img/character/${dados[count].personagem}`
+  img.src= caminho
+  console.log(caminho)
+
+  var fundo = document.getElementById("fundo")
+  if (count == 0){
+    fundo.style.backgroundImage = "url('../img/background/bg2.jpg')";
+  }else if (count == 5){
+      fundo.style.backgroundImage = "url('../img/background/bg4.jpg')";
+    } else if (count ==20){
+      fundo.style.backgroundImage = "url('../img/background/bg3.jpg')";
+    } 
+    
+
+  count ++
+  console.log(count)
+
+}
+var contador = 0
+ function mute () {
+  var turn = document.getElementById("turn")
+  var mute = document.getElementById("mudo")
+   if (contador == 0){
+      mute.pause()
+      contador = 1
+      turn.innerHTML = "Ligar Música"
+   }
+   else {
+     mute.play()
+     contador = 0
+     turn.innerHTML = "Desligar Música"
+   } 
+}
+
+function btnsound(){
+  var btnsound = document.getElementById("btnsound")
+  btnsound.play()
+}
+  
