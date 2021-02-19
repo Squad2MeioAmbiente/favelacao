95612e38c99d653e6fe452596d27e4b9f78dfd06
Bianca-AP <72463852+Bianca-AP@users.noreply.github.com>
2021-02-19 16:05:02 -0300

    Mobile - Favelação

---
+++ mobile/Pages/home/Home.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Text,  ScrollView,View, ImageBackground,TouchableOpacity, Button} from 'react-native';
+import styles from './style';
+import Fundo from '../../assets/homepage.png';
+
+
+function Home({navigation}){
+
+   
+   
+  return (
+    <View style={styles.caixa}>     
+     
+      <ImageBackground source={Fundo} style={styles.fundo}>
+        
+        <View> 
+
+          <TouchableOpacity style={styles.btn} onPress={()=>{navigation.navigate('Login')}}>
+            <Text style={styles.texto}>JOGAR</Text>
+          </TouchableOpacity>
+          
+
+        </View>
+        
+      </ImageBackground>
+   
+    </View>
+  )
+  
+}
+export default Home;
