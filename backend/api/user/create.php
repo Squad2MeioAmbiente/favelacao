d9d0d773147b4e539148642e00bcd603dc37948d
Bianca-AP <72463852+Bianca-AP@users.noreply.github.com>
2021-02-19 12:49:18 -0300

    Create user atualizado

--- backend/api/user/create.php
+++ backend/api/user/create.php
@@ -2,8 +2,8 @@
     require_once('../config/dblogincad.php');
 
 
-    if(isset($_POST['nome']) && (($_POST['senha'])===($_POST['confirmarSenha'])) ){ 
-        
+    if(isset($_POST['nome']) ){ 
+
         $nome =$_POST['nome'];
         $dataNascimento =$_POST['dataNascimento']; 
         $email =$_POST['email'];
@@ -18,11 +18,28 @@
         $results = mysqli_query($conn, $sqlverificar); 
 
         if (mysqli_num_rows($results)>=1){
-            echo "<script>
+            
+            if (($_POST['senha'])!==($_POST['confirmarSenha'])){
+        
+                echo "<script>
+                    alert ('Email já cadastrado e a senha e a confirmação devem ser iguais!')
+                    window.location.href='../../../frontend/pages/cadastro.php'
+                </script>";
+            }else{
+                echo "<script>
                 alert ('Email já cadastrado!')
                 window.location.href=''
             </script>";
-        }else{ 
+            }
+                
+        }
+        elseif (($_POST['senha'])!==($_POST['confirmarSenha'])){
+            echo "<script>
+                alert ('A senha e a confirmação devem ser iguais!')
+                window.location.href='../../../frontend/pages/cadastro.php'
+            </script>";
+        } 
+        else{ 
             $sql = "insert into usuarios (nome, dataNascimento, email, telefone, senha, confirmarSenha, apelido, imgAvatar) values ('$nome', '$dataNascimento', '$email', '$telefone', '$senha', '$confirmarSenha', '$apelido', '$imgAvatar' )"; 
             $result = mysqli_query($conn, $sql); 
             if($result && mysqli_num_rows($results)==0){ 
@@ -31,19 +48,22 @@
                     window.location.href='../../../frontend/pages/login.php'
                 </script>";
             }
+            
             else{ 
                 echo "<script>
                     alert ('Dados não inseridos!')
                     window.location.href=''
                 </script>";
             } 
-        }    
-    }
+        } 
+        
+    } 
+    
     else{
         echo "<script>
-                alert ('Dados não inseridos! A senha e a confirmação devem ser iguais')
-                window.location.href='../../../frontend/pages/cadastro.php'
-            </script>";
+            window.location.href='../../../frontend/pages/cadastro.php'
+        </script>";
+    
     } 
     
 ?>  
