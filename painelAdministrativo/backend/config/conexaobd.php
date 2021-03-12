132594b1b1b51fc5fb98ef8561bd91855a1217ed
FavelAção <73999233+Squad2MeioAmbiente@users.noreply.github.com>
2021-03-12 18:36:34 -0300

    Favelação Web com Painel Administrativo

---
+++ painelAdministrativo/backend/config/conexaobd.php
@@ -0,0 +1,10 @@
+<?php
+    $servername = "localhost"; 
+    $username = "root"; 
+    $password = ""; 
+    $database = "favelacao"; 
+    $conn = mysqli_connect($servername, $username, $password, $database); 
+    mysqli_set_charset($conn,'utf8'); 
+?>
+
+
